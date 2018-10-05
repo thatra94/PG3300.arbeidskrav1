bd2c741298c48dfcf5eb21231abaa07cf1d941bd
thatra94 <38081855+thatra94@users.noreply.github.com>
2018-10-05 19:39:34 +0200

    refactored point

--- PG3300_Innlevering_Kode/SnakeMess/Point.cs
+++ PG3300_Innlevering_Kode/SnakeMess/Point.cs
@@ -4,15 +4,16 @@
 	{
 
 		public const string Ok = "Ok";
-
 		public int X;
         public int Y;
 
-	    public Point(int x = 0, int y = 0) {
-        X = x; Y = y;
+	    public Point(int x = 0, int y = 0)
+        {
+            X = x; Y = y;
         }
-		public Point(Point input) {
-        X = input.X; Y = input.Y;
+		public Point(Point input)
+        {
+            X = input.X; Y = input.Y;
         }
 	}
 
