d0e605393f2df766fb88f218152bca3f5af745e9
thatra94 <38081855+thatra94@users.noreply.github.com>
2018-10-10 17:56:02 +0200

    d
    dsad

--- PG3300_Innlevering_Kode/SnakeMess/Food.cs
+++ PG3300_Innlevering_Kode/SnakeMess/Food.cs
@@ -9,37 +9,11 @@ namespace SnakeMess
 
     public class Food : Point
     {
-        Point newFood = new Point();
-        Random random = new Random();
-
 
         public Food(int boardWidth, int boardHeight, Snake snake)
         {
-            newFood.X = random.Next(0, boardWidth);
-            newFood.Y = random.Next(0, boardHeight);            
-            //CheckFreeSpot(snake.GetSnake());
         }
 
-       /* public void CheckFreeSpot(List<Point> snake)
-        {
-            while (true)
-            {
-                bool freeSpot = true;
-                foreach (Point i in snake)
-                    if (i.X == newFood.X && i.Y == newFood.Y)
-                    {
-                        freeSpot = false;
-                        break;
-                    }
-                if (freeSpot)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.SetCursorPosition(newFood.X, newFood.Y);
-                    Console.Write("$");
-                    break;
-                }
-            }
-        }*/
         public static void PlaceFood(int boardWidth, int boardHeight, Random random, Point food, Snake snake)
         {
             while (true)
