552194e46f10287f92b057bc946c9c298cfe6b71
Erlend Skansen <skaerl17@EGMS.no>
2018-10-10 20:40:29 +0200

    Created nonfunctional GameBoard class

--- PG3300_Innlevering_Kode/SnakeMess/SnakeMess.cs
+++ PG3300_Innlevering_Kode/SnakeMess/SnakeMess.cs
@@ -32,6 +32,7 @@ namespace SnakeMess
 
             var snake = new Snake();
 			var Controls = new Controls();
+			//var GameBoard = new GameBoard();
 
             Console.CursorVisible = false;
             Console.Title = "SNAKE";
@@ -65,7 +66,7 @@ namespace SnakeMess
                     GameOver(ref gameOver, ref inUse, boardWidth, boardHeight, random, food, snake, newHead);
                     if (!gameOver)
                     {
-                        last = NotGameOver(ref inUse, newDir, food, snake, tail, head, newHead);
+                        last = /*GameBoard.*/NotGameOver(ref inUse, newDir, food, snake, tail, head, newHead);
                     }
                 }
             }
