eccf4afb602565d4db5c066775a6852d91f86238
fredriklugg <frelugg@gmail.com>
2018-10-10 19:30:49 +0200

    fjernet kommentert kode

--- PG3300_Innlevering_Kode/SnakeMess/SnakeMess.cs
+++ PG3300_Innlevering_Kode/SnakeMess/SnakeMess.cs
@@ -140,26 +140,5 @@ namespace SnakeMess
 
             return gameOver;
         } 
-
-
-
-       /*
-        private static void Controls(ref bool gameOver, ref bool pause, ref short newDir, short last)
-        {
-            ConsoleKeyInfo cki = Console.ReadKey(true);
-            if (cki.Key == ConsoleKey.Escape)
-                gameOver = true;
-            else if (cki.Key == ConsoleKey.Spacebar)
-                pause = !pause;
-            else if (cki.Key == ConsoleKey.UpArrow && last != 2)
-                newDir = 0;
-            else if (cki.Key == ConsoleKey.RightArrow && last != 3)
-                newDir = 1;
-            else if (cki.Key == ConsoleKey.DownArrow && last != 0)
-                newDir = 2;
-            else if (cki.Key == ConsoleKey.LeftArrow && last != 1)
-                newDir = 3;
-        }
-		*/
     }
 }
